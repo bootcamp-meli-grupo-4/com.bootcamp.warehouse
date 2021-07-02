841908ad3a17abb32ddb4b60bcf56df8eab282b1
israelmercadolivre <israel.melo@mercadolivre.com>
2021-07-01 23:01:57 -0300

    rollback webSecurity

--- src/main/java/com/mercadolibre/dambetan01/Application.java
+++ src/main/java/com/mercadolibre/dambetan01/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		ScopeUtils.calculateScopeSuffix();
