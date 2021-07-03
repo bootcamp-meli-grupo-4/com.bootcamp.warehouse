ee2f1f82bf2f83a2020530c3fb1d16fbe64bf51c
israelmercadolivre <israel.melo@mercadolivre.com>
2021-07-02 21:35:55 -0300

    Init unit test

---
+++ src/test/java/com/mercadolibre/dambetan01/unit/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,132 @@
+package com.mercadolibre.dambetan01.unit.service.impl;
+
+import com.mercadolibre.dambetan01.dtos.OrderDto;
+import com.mercadolibre.dambetan01.dtos.ProductStockDto;
+import com.mercadolibre.dambetan01.dtos.SectorDto;
+import com.mercadolibre.dambetan01.dtos.response.ProductStockResponseDto;
+import com.mercadolibre.dambetan01.mapper.OrderMapper;
+import com.mercadolibre.dambetan01.mapper.ProductStockResponseMapper;
+import com.mercadolibre.dambetan01.model.*;
+import com.mercadolibre.dambetan01.model.Order;
+import com.mercadolibre.dambetan01.repository.OrderRepository;
+import com.mercadolibre.dambetan01.repository.RepresentantRepository;
+import com.mercadolibre.dambetan01.service.OrderService;
+import com.mercadolibre.dambetan01.service.RepresentantService;
+import com.mercadolibre.dambetan01.service.SectorService;
+import com.mercadolibre.dambetan01.service.impl.OrderServiceImpl;
+import com.mercadolibre.dambetan01.service.impl.ProductStockServiceImpl;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OrderServiceImplTest {
+
+    private static OrderRepository orderRepository;
+    private static OrderMapper orderMapper;
+    private static ProductStockResponseMapper productStockResponseMapper;
+    private static SectorService sectorService;
+    private static ProductStockServiceImpl productStockService;
+    private static RepresentantService representantService;
+    private static OrderService orderService;
+
+    @BeforeAll
+    public static void setUp(){
+        orderRepository = mock(OrderRepository.class);
+        orderMapper = mock(OrderMapper.class);
+        productStockResponseMapper = mock(ProductStockResponseMapper.class);
+        sectorService = mock(SectorService.class);
+        productStockService = mock(ProductStockServiceImpl.class);
+        representantService = mock(RepresentantService.class);
+        orderService = new OrderServiceImpl(orderRepository, orderMapper, productStockResponseMapper,
+                sectorService, productStockService, representantService);
+
+    }
+
+    @Test
+    public void shouldCreateOrderCorrectly(){
+        Long idRepresentant = 1l;
+        OrderDto dto = createDto();
+        Order order = mock(Order.class);
+        Sector sector = mock(Sector.class);
+        Category category = mock(Category.class);
+        Warehouse warehouse = mock(Warehouse.class);
+        ProductStock productStock = createProductStock();
+
+        ProductStockResponseDto productStockResponseDto = new ProductStockResponseDto();
+        productStockResponseDto.setCurrentQuantity(1);
+
+        when(orderMapper.dtoToModel(dto)).thenReturn(order);
+        when(sectorService.findBySectorAndWarehouse(dto.getSection().getSectionCode(), dto.getSection().getWarehouseCode()))
+                .thenReturn(sector);
+        when(sector.getWarehouse()).thenReturn(warehouse);
+        when(sector.getCategory()).thenReturn(category);
+        List<ProductStock> productStocksList = Arrays.asList(productStock);
+        when(this.productStockService.getQuantityProductsByProductStocks(productStocksList)).thenReturn(1);
+        when(order.getProductStocks()).thenReturn(productStocksList);
+        when(orderRepository.save(order)).thenReturn(order);
+        when(productStockResponseMapper.modelToDto(productStock)).thenReturn(productStockResponseDto);
+        when(productStockService.getProductStockByOrder(order)).thenReturn(productStocksList);
+        when(productStockService.saveAll(productStocksList)).thenReturn(productStocksList);
+
+        List<ProductStockResponseDto> responseDto = orderService.crateOrder(dto, idRepresentant);
+
+        Assertions.assertNotNull(responseDto);
+        Assertions.assertFalse(responseDto.isEmpty());
+        Assertions.assertEquals(1, responseDto.size());
+        Assertions.assertEquals(1, responseDto.get(0).getCurrentQuantity());
+        verify(representantService, times(1)).checkRelationBetweenRepresentantAndWarehouse(idRepresentant, warehouse);
+        verify(productStockService, times(1)).checkCategoryProductAndSector(productStocksList, category);
+        verify(sectorService, times(1)).checkSectorSpace(sector, 1);
+        verify(productStockService, times(1)).saveAll(productStocksList);
+    }
+
+    public OrderDto createDto(){
+        OrderDto dto = new OrderDto();
+        SectorDto sectorDto = new SectorDto();
+        sectorDto.setSectionCode(1l);
+        sectorDto.setWarehouseCode(1l);
+
+        dto.setOrderDate(LocalDate.now());
+        dto.setSection(sectorDto);
+
+        ProductStockDto p1 = new ProductStockDto();
+        p1.setManufacturingTime(LocalDateTime.now());
+        p1.setManufacturingDate(LocalDate.now());
+        p1.setMinimumTemperature(1d);
+        p1.setInitialQuantity(1);
+        p1.setCurrentQuantity(1);
+        p1.setCurrentTemperature(2d);
+        p1.setDueDate(LocalDate.now());
+        p1.setProductId(1l);
+        dto.setBatchStock(Arrays.asList(p1));
+
+        return dto;
+    }
+
+    public ProductStock createProductStock(){
+        ProductStock p1 = new ProductStock();
+        Product product = new Product();
+        Category category = new Category();
+        category.setName("Congelados");
+        category.setId(1l);
+        product.setCategory(category);
+
+        p1.setManufacturingTime(LocalDateTime.now());
+        p1.setManufacturingDate(LocalDate.now());
+        p1.setMinimumTemperature(1d);
+        p1.setInitialQuantity(1);
+        p1.setCurrentTemperature(2d);
+        p1.setDueDate(LocalDate.now());
+        p1.setProduct(product);
+
+        return p1;
+    }
+
+
+}
