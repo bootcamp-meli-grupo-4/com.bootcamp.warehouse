d3183f643ef0a45c50042e156367b85602250fb9
Luiz Ferreira <luiferreira@br0c02dx6jamd6n.ml.com>
2021-07-05 14:36:46 -0300

    adicionada logica necessaria para editar um purchase order e realizado alguns refatoramente para reaproveitamento de codigo

--- src/main/java/com/mercadolibre/dambetan01/service/IPurchaseOrderService.java
+++ src/main/java/com/mercadolibre/dambetan01/service/IPurchaseOrderService.java
@@ -2,7 +2,11 @@ package com.mercadolibre.dambetan01.service;
 
 import com.mercadolibre.dambetan01.dtos.purchase.CreatePurchaseOrderDTO;
 import com.mercadolibre.dambetan01.dtos.purchase.CreatePurchaseOrderResponseDTO;
+import com.mercadolibre.dambetan01.dtos.purchase.EditPurchaseOrderDTO;
+import com.mercadolibre.dambetan01.dtos.purchase.EditPurchaseOrderResponse;
 
 public interface IPurchaseOrderService {
     CreatePurchaseOrderResponseDTO createPurchaseOrder(CreatePurchaseOrderDTO createPurchaseOrderDTO, Long buyerId);
+
+    EditPurchaseOrderResponse editPurchaseOrder(EditPurchaseOrderDTO editPurchaseOrderDTO, Long purchaseOrderId, Long buyerId);
 }
