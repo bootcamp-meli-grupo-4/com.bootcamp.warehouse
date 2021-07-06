07f7b40fcd89410c82eba7012b2d49172817f04f
israelmercadolivre <israel.melo@mercadolivre.com>
2021-07-06 09:38:47 -0300

    Adjusting test orderController

--- src/test/java/com/mercadolibre/dambetan01/integration/OrderControllerTest.java
+++ src/test/java/com/mercadolibre/dambetan01/integration/OrderControllerTest.java
@@ -52,7 +52,7 @@ public class OrderControllerTest extends ControllerTestLoginMvc {
                 new ProductPurchaseOrderDTO(2L, 2)
         ));
         ResultMatcher resultMatcher = status().isOk();
-        sendPutRequest("/fresh-products/orders/" + 1, editPurchaseOrderDTO, resultMatcher);
+        sendPutRequest("/fresh-products/orders/" + 2, editPurchaseOrderDTO, resultMatcher);
     }
 
     @Test
@@ -64,7 +64,7 @@ public class OrderControllerTest extends ControllerTestLoginMvc {
                 new ProductPurchaseOrderDTO(2L, 2)
         ));
         ResultMatcher resultMatcher = status().isBadRequest();
-        sendPutRequest("/fresh-products/orders/" + 1, editPurchaseOrderDTO, resultMatcher);
+        sendPutRequest("/fresh-products/orders/" + 2, editPurchaseOrderDTO, resultMatcher);
     }
 
     @Test
@@ -76,9 +76,10 @@ public class OrderControllerTest extends ControllerTestLoginMvc {
     }
 
     @Test
+    @Order(5)
     public void do_get_purchase_order_return_200() throws Exception {
         ResultMatcher resultMatcher = status().isOk();
-        sendGetRequest("/fresh-products/orders/18", resultMatcher);
+        sendGetRequest("/fresh-products/orders/"+2, resultMatcher);
     }
 
     @Test
