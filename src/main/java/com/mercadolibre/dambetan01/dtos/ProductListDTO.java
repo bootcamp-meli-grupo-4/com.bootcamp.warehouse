de646274b5f99f4c3c05b79ff701ce4d6c9013a2
Thiago Schnr <thiago.schnr@mercadolivre.com>
2021-07-06 12:50:57 -0300

    tests product controller

--- src/main/java/com/mercadolibre/dambetan01/dtos/ProductListDTO.java
+++ src/main/java/com/mercadolibre/dambetan01/dtos/ProductListDTO.java
@@ -1,11 +1,16 @@
 package com.mercadolibre.dambetan01.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDate;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductListDTO {
 
     @JsonProperty("id")
